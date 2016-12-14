e32908a9141555783e8f9461267b71a67decb2e4
singhsonu778 <singhsonu778@gmail.com>
2016-12-14 19:37:59 +0530

    Use BeanFactory as container for spring instead of ApplicationContext

--- src/com/sonu/spring/MainApp.java
+++ src/com/sonu/spring/MainApp.java
@@ -1,14 +1,16 @@
 package com.sonu.spring;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
 
-		Message obj = (Message) context.getBean("messageBean");
+		Message obj = (Message) factory.getBean("messageBean");
 
 		obj.getMessage();
 

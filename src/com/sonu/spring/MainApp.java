066a70a15d3dfa35932926fb818eb2a8794c7bdb
singhsonu778 <singhsonu778@gmail.com>
2016-12-14 19:45:28 +0530

    Use 'init-method' and 'destroy-method' to register init and destroy method

--- src/com/sonu/spring/MainApp.java
+++ src/com/sonu/spring/MainApp.java
@@ -1,20 +1,17 @@
 package com.sonu.spring;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
 		Message obj = (Message) context.getBean("messageBean");
 
-		obj.setMessage("Hello World!");
 		obj.getMessage();
-		
-		Message obj1 = (Message) context.getBean("messageBean");
-		obj1.getMessage();
+		context.registerShutdownHook();
 
 	}
 }
--- src/com/sonu/spring/Message.java
+++ src/com/sonu/spring/Message.java
@@ -11,4 +11,12 @@ public class Message {
 	public void getMessage() {
 		System.out.println("Message : " + message);
 	}
+
+	public void init() {
+		System.out.println("In init method...");
+	}
+
+	public void destroy() {
+		System.out.println("In destroy method...");
+	}
 }

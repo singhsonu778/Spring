e4fc8ed116a472fef2db0932180b8acdc6cf7e88
singhsonu778 <singhsonu778@gmail.com>
2016-12-14 19:45:28 +0530

    Create and use Bean Definition template (abstract template)

--- src/com/sonu/spring/MainApp.java
+++ src/com/sonu/spring/MainApp.java
@@ -8,10 +8,6 @@ public class MainApp {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
-		HelloWorld world = (HelloWorld) context.getBean("world");
-		world.getName();
-		world.getMessage();
-
 		HelloCountry country = (HelloCountry) context.getBean("country");
 		country.getName();
 		country.getMessage();

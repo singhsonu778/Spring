b6e50e0635178bbf541b0203bd8c9e4eb90f1a5c
singhsonu778 <singhsonu778@gmail.com>
2016-12-14 19:45:28 +0530

    Use 'singleton' pattern for bean definition

--- src/com/sonu/spring/MainApp.java
+++ src/com/sonu/spring/MainApp.java
@@ -1,18 +1,20 @@
 package com.sonu.spring;
 
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
-		Message obj = (Message) factory.getBean("messageBean");
+		Message obj = (Message) context.getBean("messageBean");
 
+		obj.setMessage("Hello World!");
 		obj.getMessage();
+		
+		Message obj1 = (Message) context.getBean("messageBean");
+		obj1.getMessage();
 
 	}
 }

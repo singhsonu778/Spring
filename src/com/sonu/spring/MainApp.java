4f7f48155f91d756bc62f74e75c17dc71174b7f4
singhsonu778 <singhsonu778@gmail.com>
2016-12-18 15:47:41 +0530

    Use logger from commons-logging

--- src/com/sonu/spring/MainApp.java
+++ src/com/sonu/spring/MainApp.java
@@ -1,12 +1,21 @@
 package com.sonu.spring;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
+	
+	static Log log = LogFactory.getLog(MainApp.class.getName());
+	
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
+		log.info("info.....");
+		log.warn("warning.....");
+		log.error("error.....");
+		
 		Student student = context.getBean("student", Student.class);
 
 		System.out.println("--------------------------");

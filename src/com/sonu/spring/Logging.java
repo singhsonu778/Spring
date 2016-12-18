643624cb8d723fa46ac50317ae42b3dff8736c62
singhsonu778 <singhsonu778@gmail.com>
2016-12-18 13:38:40 +0530

    Use @AspectJ based AOP with Spring

--- src/com/sonu/spring/Logging.java
+++ src/com/sonu/spring/Logging.java
@@ -1,19 +1,35 @@
 package com.sonu.spring;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
 public class Logging {
 
+	@Pointcut("execution(* com.sonu.spring.*.*(..))")
+	private void selectAll() {
+	}
+
+	@Before("selectAll()")
 	public void beforeAdvice() {
 		System.out.println("Before advice...");
 	}
 
+	@After("selectAll()")
 	public void afterAdvice() {
 		System.out.println("After advice...");
 	}
 
+	@AfterReturning(pointcut = "selectAll()", returning = "value")
 	public void afterReturning(Object value) {
 		System.out.println("Returning : " + value.toString());
 	}
 
+	@AfterThrowing(pointcut = "selectAll()", throwing = "ex")
 	public void afterThrowing(IllegalArgumentException ex) {
 		System.out.println("Exception : " + ex.toString());
 	}
